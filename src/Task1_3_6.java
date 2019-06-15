ea05ce205b47a2f8a891a272eb23f62a6a4d167e
Kor6un <Zmey_85@bk.ru>
2019-06-15 11:24:06 +0300

    исправлено решение

--- src/Task1_3_6.java
+++ src/Task1_3_6.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Task1_3_6 {
     public static void main(String[] args) {
-        char[] c;
+        /*char[] c;
         System.out.println("Введите любой символ ");
         Scanner scanner = new Scanner(System.in);
         c = scanner.nextLine().toCharArray();
+        System.out.println((int)c[0]);*/
 
-        System.out.println((int)c[0]);
+        for (int i = 0; i <= 127; i++) {
+            System.out.println((char)i + " = " + i);
+        }
+
     }
 }
